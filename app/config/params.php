d86679e5423f4a3f82c05ca66f420048aceb5218
romainIEPS <romain.garcia@hotmail.com>
2020-09-08 13:58:55 +0200

    Arborescence vierge
    Mise en place de l'arborescence vierge de l'application

---
+++ app/config/params.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  ./app/config/params.php
+  PARAMETRES DE L'APPLICATION
+ */
+
+    // PARAMETRES DE CONNEXION A LA BASE DE DONNEES
+      define('HOSTNAME', 'localhost:8889');
+      define('DBNAME', '');
+      define('USERNAME', 'root');
+      define('USERPWD', 'root');
+
+    // INITIALISATION DES ZONES DYNAMIQUES
+      $content = '';
+      $title = '';
