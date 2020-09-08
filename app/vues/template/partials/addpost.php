2654a275191ced46e0836ad86cf64c1d6ec141d8
romainIEPS <romain.garcia@hotmail.com>
2020-09-08 14:46:50 +0200

    Partials + zone dynamiques
    Découpage du template en partials et initialistion des zones dynamiques

---
+++ app/vues/template/partials/addpost.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  ./app/vues/template/partials/addpost.php
+ */
+?>
+<div class="clearfix">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit placeat repudiandae maiores sequi nemo aliquid libero dolores ab debitis commodi, veniam fuga iste nulla impedit ea nam itaque optio, quibusdam.</p>
+  <p>Eveniet optio tempore beatae, nulla voluptatibus repudiandae, rem dolor, sunt neque perferendis pariatur. Placeat nobis explicabo, maxime soluta magnam ullam mollitia voluptatibus, quia in, autem molestias voluptatem ducimus fuga error.</p>
+</div>
+
+<!-- ADD A POST -->
+<div class="clearfix">
+  <a class="btn btn-secondary float-left" href="#">Add Post &rarr;</a>
+</div>
