0e578bcf5226192dd7b039f54ae10413c9a9000a
romainIEPS <romain.garcia@hotmail.com>
2020-09-15 13:57:45 +0200

    Page contact
    Mise en place du lien "contact" qui mène vers la page contact + affichage de la vue

--- app/routeur.php
+++ app/routeur.php
@@ -4,11 +4,22 @@
   ROUTEUR PRINCIPAL DE L'APPLICATION
 */
 
+  // PAGE CONTACT
+  // PATTERN: /contact
+  // CRTL: /
+  // ACTION: /
+    if(isset($_GET['contact'])):
+      GLOBAL $content, $title;
+      $title = "Contact";
+      ob_start();
+        include_once '../app/vues/contact/index.php';
+      $content = ob_get_clean();
+
   // DETAIL D'UN POST
   // PATTERN: /
   // CRTL: postsControleur
   // ACTION: index
-    if(isset($_GET['postId'])):
+    elseif(isset($_GET['postId'])):
       include_once '../app/controleurs/postsControleur.php';
       \App\Controleurs\PostsControleur\showAction($connexion, $_GET['postId']);
 
